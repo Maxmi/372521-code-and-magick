ee6fb336d73d491dc6e963f87540e8899745a42f
Gulmira Berdalieva <miramaxeast@yahoo.com>
2017-03-30 23:15:03 -0700

    Project canvas

--- js/stat.js
+++ js/stat.js
@@ -10,18 +10,18 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.shadowOffsetX = 10;
     ctx.shadowOffsetY = 10;
     ctx.shadowBlur = 10;
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'rgba(255,255,255,1)';
     ctx.fillRect(100, 10, 420, 270);
-    //ctx.save();
+
 
     //writing text
     var text1 = 'Ура, вы победили!';
     var text2 = 'Список результатов: ';
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'rgba(0,0,0,1)';
     ctx.font = '16px PT Mono';
     ctx.shadowColor = 'transparent';
-    ctx.fillText(text1, 110, 40);
-    ctx.fillText(text2, 110, 60);
+    ctx.fillText(text1, 120, 40);
+    ctx.fillText(text2, 120, 60);
 
 
     var max = -1;
@@ -36,21 +36,24 @@ window.renderStatistics = function(ctx, names, times) {
 
     var histogramHeight = 150;
     var step = histogramHeight / (max);
-
-    var initialX = 120;
-    var initialY = 80;
+    var initialX = 140;
+    var initialY = 260; //Y for names
     var barWidth = 40;
     var indent = 50;
-    var vertIndent = 20;
+    var columnY = (initialY - 20); //Y for columns
+    var scoresY = (columnY - 10) //Y for scores
 
 
     for (var i = 0; i < times.length; i++) {
-        ctx.fillText((times[i].toFixed(0)), (initialX + (barWidth * i) + (indent * i)), initialY);
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)'; //red should be for You only, others should be blue
-        ctx.fillRect((initialX + (barWidth * i) + (indent * i)), initialY, barWidth, times[i] * step);
-        //bars should be aligned at the bottom, not on the top
-        ctx.fillStyle = "black";
-        ctx.fillText(names[i], (initialX + (barWidth * i) + (indent * i)), initialY + histogramHeight + vertIndent);
+        ctx.fillText((times[i].toFixed(0)), (initialX + (barWidth * i) + (indent * i)), (scoresY - (times[i] * step)));
+
+        ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+        if (names[i] == "Вы") {
+            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+        }
+        ctx.fillRect((initialX + (barWidth * i) + (indent * i)), columnY, barWidth, -(times[i] * step));
+        ctx.fillStyle = 'rgba(0,0,0,1)';
+        ctx.fillText(names[i], (initialX + (barWidth * i) + (indent * i)), initialY);
 
     }
 }
