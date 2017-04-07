ea5981b495fe8df3c394f5141d3d58a0a5750b95
Gulmira Berdalieva <miramaxeast@yahoo.com>
2017-04-06 21:55:26 -0700

    third commit refactored the code

--- js/stat.js
+++ js/stat.js
@@ -1,29 +1,5 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  // var canvas = document.querySelector('.demo > canvas');
-
-
-  // var ctx = canvas.getContext('2d');
-
-    // drawing cloud
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowBlur = 10;
-  ctx.fillStyle = 'rgba(255,255,255,1)';
-  ctx.fillRect(100, 10, 420, 270);
-
-
-    // writing text
-  var text1 = 'Ура, вы победили!';
-  var text2 = 'Список результатов: ';
-  ctx.fillStyle = 'rgba(0,0,0,1)';
-  ctx.font = '16px PT Mono';
-  ctx.shadowColor = 'transparent';
-  ctx.fillText(text1, 120, 40);
-  ctx.fillText(text2, 120, 60);
-
-
   var max = -1;
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
@@ -41,16 +17,56 @@ window.renderStatistics = function (ctx, names, times) {
   var columnY = (initialY - 20); // Y for columns
   var scoresY = (columnY - 10); // Y for scores
 
+    // drawing cloud
+  function drawCloud() {
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowBlur = 10;
+    ctx.fillStyle = 'rgba(255,255,255,1)';
+    ctx.fillRect(100, 10, 420, 270);
+  }
 
-  for (i = 0; i < times.length; i++) {
-    ctx.fillText((times[i].toFixed(0)), (initialX + (barWidth * i) + (indent * i)), (scoresY - (times[i] * step)));
+    // writing text
+  function writeText() {
+    var text1 = 'Ура, вы победили!';
+    var text2 = 'Список результатов: ';
+    ctx.fillStyle = 'rgba(0,0,0,1)';
+    ctx.font = '16px PT Mono';
+    ctx.shadowColor = 'transparent';
+    ctx.fillText(text1, 120, 40);
+    ctx.fillText(text2, 120, 60);
+  }
 
+  function getX(index) {
+    return initialX + (barWidth * index) + (indent * index);
+  }
+
+  function drawScore(index) {
+    ctx.fillText((times[index].toFixed(0)),
+        getX(index),
+        (scoresY - (times[index] * step)));
+  }
+
+  function drawColumn(index) {
     ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    ctx.fillRect((initialX + (barWidth * i) + (indent * i)), columnY, barWidth, -(times[i] * step));
+    ctx.fillRect(getX(index), columnY, barWidth, -(times[index] * step));
+  }
+
+  function drawNames(index) {
     ctx.fillStyle = 'rgba(0,0,0,1)';
-    ctx.fillText(names[i], (initialX + (barWidth * i) + (indent * i)), initialY);
+    ctx.fillText(names[index], getX(index), initialY);
+  }
+
+  drawCloud();
+  writeText();
+
+  for (i = 0; i < times.length; i++) {
+    drawScore(i);
+    drawColumn(i);
+    drawNames(i);
   }
 };
